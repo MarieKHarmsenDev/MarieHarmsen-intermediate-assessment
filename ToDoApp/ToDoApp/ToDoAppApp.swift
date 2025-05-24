4e08932941ddace58f7cb69b5203797c0381cb27
Marie Harmsen <blazingbodylife@gmail.com>
2025-05-24 22:09:36 +0100

    Initial commit

---
+++ ToDoApp/ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Marie Harmsen on 24/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToDoApp/ToDoAppTests/ToDoAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  ToDoAppTests.swift
+//  ToDoAppTests
+//
+//  Created by Marie Harmsen on 24/05/2025.
+//
+
+import Testing
+
+struct ToDoAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
